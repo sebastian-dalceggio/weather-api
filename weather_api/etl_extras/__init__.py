4e4bed9de3d860548ba7fa399dbe34e9e8fd0fee
Sebastián Dalceggio <sdalceggio@gmail.com>
2024-03-07 10:25:33 -0300

    Added sensor.

--- weather_api/etl_extras/__init__.py
+++ weather_api/etl_extras/__init__.py
@@ -1,4 +1,4 @@
 """Extra functions for the ETL process. This functions doesn't use the etl group of
 dependencies."""
 
-from weather_api.etl_extras.utils import get_run_data
+from weather_api.etl_extras.utils import get_run_data, check_file_availability
