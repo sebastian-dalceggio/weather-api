7b35ae7e853b858c37422c2d95e18427f9e74668
Sebastián Dalceggio <sdalceggio@gmail.com>
2024-03-07 20:52:37 -0300

    Added etl functions to __init__

--- weather_api/etl/__init__.py
+++ weather_api/etl/__init__.py
@@ -1,2 +1,4 @@
 """Actual functions that Airflow will import to run the ETL process. They translate the query name
 into the specfic class"""
+
+from weather_api.etl.etl import download, validate_raw, to_csv
